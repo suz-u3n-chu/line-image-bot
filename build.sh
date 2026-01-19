8a9b35d1ba3a7757f49e620749f7a2fe37af2cd1
Kaito <suz.u3n.chu@gmail.com>
2026-01-19 10:56:16 +0900

    Add build.sh to fix Render build errors

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Upgrade pip and build tools
+pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+pip install -r requirements.txt
